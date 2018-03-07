b8bdce1122a34de264ceae4a85f0aa5910bd86a7
Mehmet Efe Akengin <mefeakengin@dhcp-18-189-18-187.dyn.MIT.EDU>
2018-03-07 03:14:24 -0500

    some changes

--- data/download.py
+++ data/download.py
@@ -3,27 +3,6 @@ import urllib2
 import zipfile
 import gzip
 
-main_dir = os.getcwd()
-
-# populate data links of format ftp://ftp.fec.gov/FEC/2018/cm18.zip
-def setup_paths():
-	years = [str(i) for i in range(1980, 2020, 2)]
-	data_prefixes = ['cm', 'cn', 'ccl', 'oth', 'pas2', 'indiv'] #operating expenditures are ignored
-	web_source = 'ftp://ftp.fec.gov/FEC'
-	data_dir = main_dir + '/fec'
-
-	if not os.path.exists(data_dir):
-		os.mkdir(data_dir)
-
-	urls = []
-	filenames = []
-	for year in years:
-		for prefix in data_prefixes:
-			if prefix != 'ccl' or int(year) >= 2000: #CCL only exists after 2000
-				urls.append(web_source + '/' + year + '/' + prefix + year[2:] + '.zip')
-				filenames.append(prefix + year[2:])
-	print filenames
-
 #Download and write the file
 def download_file(url, path):
 	connection = urllib2.urlopen(url)
@@ -52,22 +31,37 @@ def clean_file(path):
 # 	clean_file(final_path)
 # 	print "File " + filename + " is cleaned"
 
-def main():
-	setup_paths()
-	# Download and extract all files
-	for i in range(0, len(urls)):
-		url = urls[i]
-		filename = filenames[i]
-		print url
-		print filename
-		final_path = data_dir + '/' + filename 
-		download_path = final_path + '.zip'
-		download_file(url, download_path)
-		extract_file(download_path, final_path)
-		clean_file(final_path)
-		os.remove(download_path)
-		print "File " + filename + " is ready"
-	
+main_dir = os.getcwd()
 
-if __name__ == '__main__':
-    main()
+# populate data links of format ftp://ftp.fec.gov/FEC/2018/cm18.zip
+years = [str(i) for i in range(1980, 2020, 2)]
+data_prefixes = ['cm', 'cn', 'ccl', 'oth', 'pas2', 'indiv'] #operating expenditures are ignored
+web_source = 'ftp://ftp.fec.gov/FEC'
+data_dir = main_dir + 'data/fec'
+
+if not os.path.exists(data_dir):
+	os.mkdir(data_dir)
+
+urls = []
+filenames = []
+for year in years:
+	for prefix in data_prefixes:
+		if prefix != 'ccl' or int(year) >= 2000: #CCL only exists after 2000
+			urls.append(web_source + '/' + year + '/' + prefix + year[2:] + '.zip')
+			filenames.append(prefix + year[2:])
+print filenames
+
+# Download and extract all files
+for i in range(0, len(urls)):
+	url = urls[i]
+	filename = filenames[i]
+	print url
+	print filename
+	final_path = data_dir + '/' + filename 
+	download_path = final_path + '.zip'
+	download_file(url, download_path)
+	extract_file(download_path, final_path)
+	clean_file(final_path)
+	os.remove(download_path)
+	print "File " + filename + " is ready"
+	
